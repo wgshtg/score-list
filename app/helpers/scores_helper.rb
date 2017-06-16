d42fa52b285d6126cf8b501f08df69c355bdcb2b
championshipnwang <championshipnwang@gmail.com>
2017-06-16 04:24:59 -0400

    add new Viewerhelper

--- app/helpers/scores_helper.rb
+++ app/helpers/scores_helper.rb
@@ -1,7 +1,16 @@
 module ScoresHelper
   def display_name(score)
-#     if score.score >= 90
-#     putc score
-    "S級"({score.score+1})
+    s = score.score.to_i
+    if s >= 90
+      "S級(#{score.score})"
+    elsif s>= 80 && s <=89
+      "A級(#{score.score})"
+    elsif s>=70 && s<=79
+      "B級(#{score.score})"
+    elsif s>=60 && s<=69
+      "C級(#{score.score})"
+    elsif s<60
+      "D級(#{score.score})"
+    end
   end
 end
